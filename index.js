244c12b166a4b1605d77c4732dc5809c9825d298
muhindinewton <muhindinewton@gmail.com>
2025-03-22 13:37:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+
+};
+
+function createEmployeeRecords (Arrays) {
+    return Arrays.map(createEmployeeRecord);
+};
+
+function createTimeInEvent (employee, datetime) {
+    let [date, hour] = datetime.split(" ");
+    employee.timeInEvents.push({type: "TimeIn", date, hour: parseInt(hour) });
+    return employee;
+}
+
+function createTimeOutEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour) });
+    return employee;
+}
+
+function hoursWorkedOnDate(employee, date) {
+    let timeIn = employee.timeInEvents.find(e => e.date === date);
+    let timeOut = employee.timeOutEvents.find(e => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;  // Convert 1400 to 14 hours
+}
+
+function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+
+function allWagesFor(employee) {
+    return employee.timeInEvents.reduce((total, event) => {
+        return total + wagesEarnedOnDate(employee, event.date);
+    }, 0);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+}
